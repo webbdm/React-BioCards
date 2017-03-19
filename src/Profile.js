c140c87b2f9c027e57473478252c1ac75a6dc8a0
webbdm <gwebb762@gmail.com>
2017-03-18 21:53:26 -0500

    added base components

--- src/Profile.js
+++ src/Profile.js
@@ -3,20 +3,46 @@ import React, { Component } from 'react';
 import './Camp.css';
 // import './Camp.js';
 
+class Profile extends Component {
+   render(props){
+    return(
+      <div className="Wrapper">
+        <Resume jobs={this.props.jobs}/>
+        <Camp jobs={this.props.jobs}/>
+      </div>
+
+    );
+  }
+}
+
+class Resume extends Component{
+  render(){
+    return(
+      <div className="resume">
+         <div className="my-info">
+         <MyInfo/>
+         </div>
+         <div className="experience">
+         <MyXP/>
+         </div>
+      </div>
+    );
+  }
+}
 
 class Camp extends Component {
    render(){
     return(
       <div className="Center">
         <div className="Card">
-          <h1>React</h1>
-          <img src={"this.props.jobs[0].mlogo"} className="Card-image" alt="logo" />
-          <h2>{this.jobs.name}</h2>
+          <h1></h1>
+          <img src={this.props.jobs[0].mlogo} className="Card-image" alt="logo" />
+          <h2>{this.props.jobs[0].name}</h2>
           <div className="Info-card">
-            <h3>{this.jobs.title}</h3>
+            <h3>{this.props.jobs[0].title}</h3>
             <div className="Info">
-              <p>{this.jobs.date}</p>
-              <p>{this.jobs.info}</p>
+              <p>{this.props.jobs[0].date}</p>
+              <p>{this.props.jobs[0].info}</p>
             </div>
           </div>
         </div>
@@ -25,14 +51,37 @@ class Camp extends Component {
   }
 }
 
-class Profile extends Component {
-   render(props){
+class MyInfo extends Component {
+   render(){
     return(
-      <div className="wrapper">
-        <h1>Hello Test</h1>
-        <Camp jobs={this.props}/>
+      <div className="myInfo">
+        <h2>Geoff Webb</h2>
+        <img src="https://media.licdn.com/media/p/2/005/02a/1c1/0e167c8.jpg" alt="Geoff"/>
+        <h3>Title</h3>
+        <p>Belmont University</p>
+        <p>Bio</p>
       </div>
+    );
+  }
+}
 
+class MyXP extends Component {
+   render(){
+    return(
+      <div className="myXP">
+        <h1>Experience</h1>
+        <div className="jobDiv">
+
+          <div className="jobLine">
+            <h3>Company</h3>
+            <h4>Title</h4>
+            <p>Spring 2014</p>
+            <p>Responsibilities</p>
+          </div>
+
+
+        </div>
+      </div>
     );
   }
 }

a1087cd370412e4d5268c3a0ee87b306d1771ed4
webbdm <gwebb762@gmail.com>
2017-04-14 20:01:53 -0500

    Made Resume an ES6 class and stateful component

--- src/Profile.js
+++ src/Profile.js
@@ -9,17 +9,54 @@ const Profile = (props) =>
         <Resume jobs={props.jobs}/>
       </div>;
 
-const Resume = (props) =>
-      <div className="resume">
-         <div className="nav">{/*GEOFF <span>WEB</span>B*/}</div>
-         <div className="my-info">
-            <MyInfo/>
-         </div>
-         <div className="experience">
-            <MyXP/>
-         </div>
-            <Camp jobs={props.jobs}/>
-      </div>;
+class Resume extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        index: 0
+      };
+    }
+
+    currentJob() {
+      console.log('test');
+      this.setState({
+        index: 0
+      })
+    }
+
+    jobTwo() {
+      this.setState({
+        index: 1
+      })
+    }
+
+    jobThree() {
+      this.setState({
+        index: 2
+      })
+    }
+
+    jobFour() {
+      this.setState({
+        index: 3
+      })
+    }
+
+    render(){
+      return(
+        <div className="resume">
+           <div className="nav">{/*GEOFF <span>WEB</span>B*/}</div>
+           <div className="my-info">
+              <MyInfo/>
+           </div>
+           <div className="experience">
+              <MyXP func={this.currentJob}/>
+           </div>
+              <Camp jobs={this.props.jobs}/>
+        </div>
+      );
+   }
+}
 
 const Camp = (props) =>
       <div className="Center">
@@ -43,7 +80,7 @@ const MyInfo = (props) =>
         <img src="https://media.licdn.com/media/p/2/005/02a/1c1/0e167c8.jpg" alt="Geoff"/>
         <h3></h3>
         <div className="logoDiv">
-          <img src="https://angular.io/resources/images/logos/angular2/angular.svg" alt="Angular"/>
+          <img src="https://angular.io/resources/images/logos/angular/angular.png" alt="Angular"/>
           <img src="http://tomwayson.github.io/mwd-jsapi/img/react-logo.png" alt="React"/>
           <img src="https://dt-cdn.net/assets/images/gfx/icons/tech/dotnetmicrosoft-536938fee2.svg" alt=".NET"/>
         </div>
@@ -58,12 +95,12 @@ const MyInfo = (props) =>
         </div>
       </div>;
 
-const MyXP = () =>
+const MyXP = (props) =>
       <div className="myXP">
         <h1>Experience</h1>
         <div className="jobDiv">
 
-          <div className="jobLine">
+          <div className="jobLine" onClick={props.func}>
             <h3>Gilbert McLaughlin Casella Architects</h3>
             <p>Marketing Coordinator</p>
             <p>Feb. 2015 - Present</p>

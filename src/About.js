a0d82d91461f1aeb87b1b95c6fb9086a7a9ea6b6
webbdm <gwebb762@gmail.com>
2017-09-12 00:22:43 -0500

    CSS Grid added too About Page

--- src/About.js
+++ src/About.js
@@ -3,12 +3,10 @@ import './Camp.css';
 
 const About = ({ }) =>
     <div className="about-wrapper">
-        <div className="about-box">
-            <div className="about-item">
-                <img alt="About" />
-                <p>This is my bio</p>
-            </div>
+        <div className="about-img">
+            <img src="https://media.licdn.com/media/p/2/005/02a/1c1/0e167c8.jpg" alt="About" />
         </div>
+        <p>This allows your columns to flex in size, ranging from 186px to equal-width columns stretching across the full width of the container. auto-fill will create as many columns as will fit in the width. If, say, five columns fit, even though you have only four grid items, there will be a fifth empty column:</p>
     </div>;
 
 export default About;

7d20cf898d6ca2c7b53a251fc862b8223c319ef1
webbdm <gwebb762@gmail.com>
2017-10-04 01:26:36 -0500

    Separated Project into individual component views

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import { HashRouter, Route, Link } from 'react-router-dom';
 //import axios from 'axios';
 import './Camp.css';
 import Profile from './Profile.js';
+import { projectData } from './ProjectData.js';
 // import './Camp.js';
 
 // Components
@@ -33,14 +34,29 @@ class HomePage extends React.Component {
                         </ul>
                     </nav>
 
-                    <div className="">
+                    {/* <div className="home-banner">
+                        <h1>Visually-minded developer with a passion for solving puzzles</h1>
+                    </div> */}
+
+                    <div className="routes">
                         <Route exact path="/resume" component={() => (<Profile jobs={this.props.j} bio={this.props.b} />)} />
                         <Route exact path="/projects" component={() => (<Projects />)} />
                         <Route exact path="/about" component={() => (<About />)} />
                         <Route exact path="/contact" component={() => (<Contact />)} />
-                        <Route exact path="/Trek" component={() => (<ProjectView />)} />
-                        <Route exact path="/MacroTracker" component={() => (<ProjectView />)} />
-                        <Route exact path="/TIL" component={() => (<ProjectView />)} />
+                        <Route exact path="/Trek" component={() => (<ProjectView name={projectData.projects[0].name}
+                            description={projectData.projects[0].description}
+                            imgUrl={projectData.projects[0].img_url}
+                            prjUrl={projectData.projects[0].url} />)} />
+
+                        <Route exact path="/MacroTracker" component={() => (<ProjectView name={projectData.projects[1].name}
+                            description={projectData.projects[1].description}
+                            imgUrl={projectData.projects[1].img_url}
+                            prjUrl={projectData.projects[1].url} />)} />
+
+                        <Route exact path="/TIL" component={() => (<ProjectView name={projectData.projects[2].name}
+                            description={projectData.projects[2].description}
+                            imgUrl={projectData.projects[2].img_url}
+                            prjUrl={projectData.projects[2].url} />)} />
                         {/* <Profile jobs={this.props.j} bio={this.props.b} /> */}
                     </div>
 
@@ -51,7 +67,7 @@ class HomePage extends React.Component {
                             <a href="https://twitter.com/GWebb762"><img alt="Twitter" src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a>
                             <a href="https://www.linkedin.com/in/geoff-webb-85637586/"><img alt="LinkedIn" src="https://simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" /></a>
                             <a href="https://github.com/webbdm"><img alt="GitHub" src="http://static.wixstatic.com/media/00a2c0_25e4011be9c24da78970d53033b0b16a~mv2.png" alt="GitHub" /></a>
-                            <span className="footer-right">Built with<img src="http://tomwayson.github.io/mwd-jsapi/img/react-logo.png"/></span>
+                            <span className="footer-right">Built with<img src="http://tomwayson.github.io/mwd-jsapi/img/react-logo.png" /></span>
                             {/* <a href="mailto:gwebb762@gmail.com">Email</a> */}
                         </div>
                     </footer>
--- src/ProjectView.js
+++ src/ProjectView.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Camp.css';
 
-const ProjectView = ({ }) =>
+const ProjectView = ({ name, description, imgUrl, prjUrl }) =>
     <div className="project-view-wrapper">
-        <h1>Project Name</h1>
+        <div className="project-view-image">
+            <a href=""><img src={imgUrl} alt="name" /></a>
+        </div>
+
+        <div className="project-view-info">
+            <h1>{name}</h1>
+            <p>{description}</p>
+        </div>
     </div>;
 
 export default ProjectView;

9ab5f2be5a2119e03ade730f813cc2386ea9ae8e
webbdm <gwebb762@gmail.com>
2017-10-04 19:52:34 -0500

    Populated About page

--- src/About.js
+++ src/About.js
@@ -6,7 +6,14 @@ const About = ({ }) =>
         <div className="about-img">
             <img src="https://media.licdn.com/media/p/2/005/02a/1c1/0e167c8.jpg" alt="About" />
         </div>
-        <p>This allows your columns to flex in size, ranging from 186px to equal-width columns stretching across the full width of the container. auto-fill will create as many columns as will fit in the width. If, say, five columns fit, even though you have only four grid items, there will be a fifth empty column:</p>
+
+        <div className="about-info">
+            <p>Born and raised in Wichita, Kansas, I grew up making trips out West hiking & snowboarding through the Rockies. I also had a passion for music & entertainment, which led me to Nashville in 2011 when I enrolled at Belmont University for music business. When I discovered the music &  incredible outdoor adventures Tennessee had to offer, I knew it was the place for me.</p>
+
+            <p>Working in entertainment marketing, I found myself assisting with & managing numerous artist and brand websites. The moment I started delving into the code, it sparked an instant zeal to learn the craft of creating visually impressive and immersive websites.</p>
+
+            <p>After 2 years of continued work in marketing, web development became something I did not because it was work, but because it was my passion. To me, it’s like solving a giant puzzle or having a Lego set with endless possibilities. This led me to enroll at Nashville Software School for 2017 and sharpen my skills as a full-stack developer. </p>
+        </div>
     </div>;
 
 export default About;
